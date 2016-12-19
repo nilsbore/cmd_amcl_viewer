2854511b6ad371b6426ec02a330555c6b4ee1fad
Nils Bore <nilsbore@gmail.com>
2016-12-19 15:41:19 +0100

    Got something with waypoints

--- src/amcl_viewer_node.cpp
+++ src/amcl_viewer_node.cpp
@@ -220,8 +220,8 @@ public:
             tie(pose_x, pose_y, direction) = pose_to_discrete_pose(wp.second);
             int flip_pose_x = subsampled_width - pose_x - 1;
             subsampled_map.at<char>(pose_y, flip_pose_x) = 'x';
-            for (int x = pose_x + 1; x < subsampled_width; ++x) {
-                subsampled_map.at<char>(pose_y, x) = wp.first[x-pose_x-1];
+            for (int x = flip_pose_x - 1; x >= 0; --x) {
+                subsampled_map.at<char>(pose_y, x) = wp.first[flip_pose_x - x + 1];
             }
         }
 
